3a978f1a4ec023d11a919d54656d64b04e09c4fb
Ziyan Li <liziyan1992@gmail.com>
2023-11-29 15:58:12 +0300

    hello controller

---
+++ OnlineOrder/src/main/java/com/laioffer/onlineorder/hello/HelloController.java
@@ -0,0 +1,39 @@
+package com.laioffer.onlineorder.hello;
+
+
+import com.github.javafaker.Faker;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        Faker faker = new Faker();
+        String name = faker.name().fullName();
+        String company = faker.company().name();
+        String street = faker.address().streetAddress();
+        String city = faker.address().city();
+        String state = faker.address().state();
+        String bookTitle = faker.book().title();
+        String bookAuthor = faker.book().author();
+
+
+        String template = "This is %s\n" +
+                "I work at %s\n" +
+                "I live at %s in %s %s\n" +
+                "My favorite book is %s by %s\n";
+        return template.formatted(
+                name,
+                company,
+                street,
+                city,
+                state,
+                bookTitle,
+                bookAuthor
+        );
+    }
+}
