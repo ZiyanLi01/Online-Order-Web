b621c61766f0bbd107d18bdd12e0126f64717e20
Ziyan Li <liziyan1992@gmail.com>
2023-11-29 20:16:33 +0300

    HelloController

--- OnlineOrder/src/main/java/com/laioffer/onlineorder/hello/HelloController.java
+++ OnlineOrder/src/main/java/com/laioffer/onlineorder/hello/HelloController.java
@@ -3,7 +3,11 @@ package com.laioffer.onlineorder.hello;
 
 import com.github.javafaker.Faker;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.Locale;
 
 
 @RestController
@@ -11,8 +15,12 @@ public class HelloController {
 
 
     @GetMapping("/hello")
-    public String sayHello() {
-        Faker faker = new Faker();
+    public Person sayHello(@RequestParam(required = false) String locale) {
+        if (locale == null) {
+            locale = "en_US";
+        }
+
+        Faker faker = new Faker(new Locale(locale));
         String name = faker.name().fullName();
         String company = faker.company().name();
         String street = faker.address().streetAddress();
@@ -22,18 +30,6 @@ public class HelloController {
         String bookAuthor = faker.book().author();
 
 
-        String template = "This is %s\n" +
-                "I work at %s\n" +
-                "I live at %s in %s %s\n" +
-                "My favorite book is %s by %s\n";
-        return template.formatted(
-                name,
-                company,
-                street,
-                city,
-                state,
-                bookTitle,
-                bookAuthor
-        );
+        return new Person(name, company, new Address(street, city, state, null), new Book(bookTitle, bookAuthor));
     }
 }
