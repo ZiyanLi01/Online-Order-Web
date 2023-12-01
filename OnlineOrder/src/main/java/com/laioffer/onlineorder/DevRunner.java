9d0c556568102d590d89cbaf648766dd43349528
Ziyan Li <liziyan1992@gmail.com>
2023-12-01 21:13:38 +0300

    add controller

--- OnlineOrder/src/main/java/com/laioffer/onlineorder/DevRunner.java
+++ OnlineOrder/src/main/java/com/laioffer/onlineorder/DevRunner.java
@@ -6,11 +6,17 @@ import com.laioffer.onlineorder.entity.CustomerEntity;
 import com.laioffer.onlineorder.entity.MenuItemEntity;
 import com.laioffer.onlineorder.entity.OrderItemEntity;
 import com.laioffer.onlineorder.entity.RestaurantEntity;
+import com.laioffer.onlineorder.model.RestaurantDto;
 import com.laioffer.onlineorder.repository.CartRepository;
 import com.laioffer.onlineorder.repository.CustomerRepository;
 import com.laioffer.onlineorder.repository.MenuItemRepository;
 import com.laioffer.onlineorder.repository.OrderItemRepository;
 import com.laioffer.onlineorder.repository.RestaurantRepository;
+import com.laioffer.onlineorder.service.CartService;
+import com.laioffer.onlineorder.service.MenuItemService;
+import com.laioffer.onlineorder.service.RestaurantService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -23,6 +29,10 @@ import java.util.List;
 public class DevRunner implements ApplicationRunner {
 
 
+    // A logger for print
+    private static final Logger logger = LoggerFactory.getLogger(DevRunner.class);
+
+
     private final CartRepository cartRepository;
     private final CustomerRepository customerRepository;
     private final MenuItemRepository menuItemRepository;
@@ -30,17 +40,29 @@ public class DevRunner implements ApplicationRunner {
     private final RestaurantRepository restaurantRepository;
 
 
+    private final CartService cartService;
+    private final MenuItemService menuItemService;
+    private final RestaurantService restaurantService;
+
+
     public DevRunner(
             CartRepository cartRepository,
             CustomerRepository customerRepository,
             MenuItemRepository menuItemRepository,
             OrderItemRepository orderItemRepository,
-            RestaurantRepository restaurantRepository) {
+            RestaurantRepository restaurantRepository,
+            CartService cartService,
+            MenuItemService menuItemService,
+            RestaurantService restaurantService
+    ) {
         this.cartRepository = cartRepository;
         this.customerRepository = customerRepository;
         this.menuItemRepository = menuItemRepository;
         this.orderItemRepository = orderItemRepository;
         this.restaurantRepository = restaurantRepository;
+        this.cartService = cartService;
+        this.menuItemService = menuItemService;
+        this.restaurantService = restaurantService;
     }
 
 
@@ -80,23 +102,37 @@ public class DevRunner implements ApplicationRunner {
         menuItemRepository.saveAll(newMenuItems);
 
 
-        List<OrderItemEntity> newOrderItems = List.of(
-                new OrderItemEntity(null, 1L, 1L, 1.0, 1),
-                new OrderItemEntity(null, 3L, 1L, 10.0, 1),
-                new OrderItemEntity(null, 5L, 1L, 20.0, 1),
-                new OrderItemEntity(null, 7L, 1L, 200.0, 1),
-                new OrderItemEntity(null, 9L, 1L, 30.0, 1),
-                new OrderItemEntity(null, 1L, 2L, 1.0, 1),
-                new OrderItemEntity(null, 2L, 2L, 10.0, 1),
-                new OrderItemEntity(null, 3L, 2L, 20.0, 1),
-                new OrderItemEntity(null, 4L, 2L, 200.0, 1),
-                new OrderItemEntity(null, 5L, 2L, 30.0, 1)
-        );
-        orderItemRepository.saveAll(newOrderItems);
-
-
         customerRepository.deleteById(2L);
         restaurantRepository.deleteById(4L);
         customerRepository.updateNameByEmail("user_a@mail.com", "first", "last");
+
+
+        // The following is new code for service
+        List<RestaurantDto> restaurantDtos = restaurantService.getRestaurants();
+        logger.info(restaurantDtos.toString());
+
+
+        List<MenuItemEntity> menuItemEntities = menuItemService.getMenuItemsByRestaurantId(2L);
+        logger.info(menuItemEntities.toString());
+
+
+        logger.info(menuItemService.getMenuItemById(1L).toString());
+
+
+        cartService.addMenuItemToCart(1L, 1L);
+        cartService.addMenuItemToCart(1L, 3L);
+        cartService.addMenuItemToCart(1L, 3L);
+        cartService.addMenuItemToCart(1L, 3L);
+        cartService.addMenuItemToCart(1L, 5L);
+        cartService.addMenuItemToCart(1L, 5L);
+
+
+        logger.info(cartService.getCart(1L).toString());
+
+
+        cartService.clearCart(1L);
+
+
+        logger.info(cartService.getCart(1L).toString());
     }
 }
