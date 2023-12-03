15142133356cf5f7a851ae2bdab550171958d7b5
Ziyan Li <liziyan1992@gmail.com>
2023-12-03 09:40:07 +0800

    compile frontend webpack

--- OnlineOrder/src/main/java/com/laioffer/onlineorder/model/RegisterBody.java
+++ OnlineOrder/src/main/java/com/laioffer/onlineorder/model/RegisterBody.java
@@ -1,13 +1,9 @@
 package com.laioffer.onlineorder.model;
 
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-
 public record RegisterBody(
         String email,
         String password,
-        @JsonProperty("first_name") String firstName,
-        @JsonProperty("last_name") String lastName
+        String firstName,
+        String lastName
 ) {
 }
